fe7f24a9ff1ddd7175a8071cbe6407a7f756fb05
ShantanuPhadke1996 <shantanu.phadke@berkeley.edu>
2020-01-05 12:38:56 -0800

    Initial commit, helloworld and some variable basics

---
+++ go/src/variables-intro/variables-intro.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 29 // variable declaration + initialization in a single line example
+	fmt.Println("my age is ", age)
+	//age = 29 // assigning variable value 1
+	fmt.Println("my age is still", age)
+	age = 54 // assigning variable value 2
+	fmt.Println("my updated age is ", age)
+
+	fmt.Println("") // free line
+
+	
+}
